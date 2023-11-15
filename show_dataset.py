759e3917c0076d7d4a6ca07af38d6161ce9abc5f
Yi Yang <yy4022@ic.ac.uk>
2023-11-15 16:31:55 +0800

    implement: show_specifiedImage() function in show_dataset file

--- show_dataset.py
+++ show_dataset.py
@@ -14,10 +14,10 @@ show_start = 6500
 show_end = 6510
 
 # PART 2: load the datasets
-with h5py.File('data/IA_PIV.mat', 'r') as file:
+with h5py.File('data/ID_PIV.mat', 'r') as file:
 
     """
-    Get the PIV numpy array with the shape of (3, 5000, 73, 73), note that:
+    Get the PIV numpy array with the shape of (3, 6689, 73, 73), note that:
         1 denotes the axial(x) velocity, 
         2 denotes the radial(y) velocity, 
         3 denotes the tangential(z) velocity
@@ -44,8 +44,7 @@ with h5py.File('data/ID_PLIF_5001to6689.mat', 'r') as file:
 
 # PART 3: define the function for showing the mean image
 def show_meanImage():
-    mean_PIV = np.mean(dataset_PIV, axis=1)
-    # mean_PIV = np.transpose(mean_PIV) # (73, 73, 3)
+    mean_PIV = np.mean(dataset_PIV, axis=1)  # (3, 73, 73)
     mean_PLIF = np.mean(dataset_PLIF, axis=0) # (608, 409)
 
     plt.figure(figsize=(28, 8))
@@ -63,7 +62,7 @@ def show_meanImage():
     # show the mean PIV-x image
     plt.subplot(1, 4, 2)
     plt.title('The Mean Image of PIV-x')
-    plt.imshow(mean_PIV[0][:][:], cmap='turbo', extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
+    plt.imshow(mean_PIV[0][:][:], extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
                origin='lower')
     sm = plt.cm.ScalarMappable(norm=norm)
     sm.set_array([])
@@ -72,7 +71,7 @@ def show_meanImage():
     # show the mean PIV-y image
     plt.subplot(1, 4, 3)
     plt.title('The Mean Image of PIV-y')
-    plt.imshow(mean_PIV[1][:][:], cmap='turbo', extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
+    plt.imshow(mean_PIV[1][:][:], extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
                origin='lower')
     sm = plt.cm.ScalarMappable(norm=norm)
     sm.set_array([])
@@ -81,7 +80,50 @@ def show_meanImage():
     # show the mean PIV-z image
     plt.subplot(1, 4, 4)
     plt.title('The Mean Image of PIV-z')
-    plt.imshow(mean_PIV[2][:][:], cmap='turbo', extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
+    plt.imshow(mean_PIV[2][:][:], extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
+               origin='lower')
+    sm = plt.cm.ScalarMappable(norm=norm)
+    sm.set_array([])
+    plt.colorbar()
+
+    plt.show()
+
+# PART 4: define the function for showing the specified image
+def show_specifiedImage():
+    plt.figure(figsize=(28, 8))
+    norm = mpl.colors.Normalize(vmin=0.0, vmax=1.0)
+
+    # show the specified PLIF image
+    plt.subplot(1, 4, 1)
+    plt.title(f'The PLIF Image {show_num}')
+    plt.imshow(dataset_PLIF[show_num-first_PLIF_num][:][:], cmap='hot', extent=(PLIF_x.min(), PLIF_x.max(), PLIF_y.min(), PLIF_y.max()),
+               origin='lower')
+    sm = plt.cm.ScalarMappable(norm=norm)
+    sm.set_array([])
+    plt.colorbar()
+
+    # show the mean PIV-x image
+    plt.subplot(1, 4, 2)
+    plt.title('The Mean Image of PIV-x')
+    plt.imshow(dataset_PIV[0][show_num-1][:][:], extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
+               origin='lower')
+    sm = plt.cm.ScalarMappable(norm=norm)
+    sm.set_array([])
+    plt.colorbar()
+
+    # show the mean PIV-y image
+    plt.subplot(1, 4, 3)
+    plt.title('The Mean Image of PIV-y')
+    plt.imshow(dataset_PIV[1][show_num-1][:][:], extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
+               origin='lower')
+    sm = plt.cm.ScalarMappable(norm=norm)
+    sm.set_array([])
+    plt.colorbar()
+
+    # show the mean PIV-z image
+    plt.subplot(1, 4, 4)
+    plt.title('The Mean Image of PIV-z')
+    plt.imshow(dataset_PIV[2][show_num-1][:][:], extent=(PIV_x.min(), PIV_x.max(), PIV_y.min(), PIV_y.max()),
                origin='lower')
     sm = plt.cm.ScalarMappable(norm=norm)
     sm.set_array([])
@@ -90,7 +132,6 @@ def show_meanImage():
     plt.show()
 
 
-
-
 # PART : the main part which calling the related functions for showing the datasets
-show_meanImage()
+# show_meanImage()
+show_specifiedImage()
