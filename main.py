fc37861c949064052b4a257b225d9c6fd33841e7
Yi Yang <yy4022@ic.ac.uk>
2023-11-17 14:00:00 +0800

    update: the main file and preprocess_old_data() function in preprocess_dataset file

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import h5py
+import numpy as np
+import scipy.io
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+from preprocess_dataset import preprocess_data, preprocess_old_data, show_image
 
 # PART 1: define the parameters
-file_PIV = str('data/IA_PIV.mat')
-file_PLIF = str('data/IA_PLIF_1to2500.mat')
+file_PIV = str('data/Lifted state/D1F1_air240_PIV_13601to14000.mat')
+file_PLIF = str('data/Lifted state/D1F1_air240_PLIF_13601to14000.mat')
+# file_PIV = str('data/IA_PIV.mat')
+# file_PLIF = str('data/IA_PLIF_1to2500.mat')
+
+# PART 2: preprocess the datasets
+# 1. concatenate the datasets as required
+
+# 2. preprocess the data, then return the cropped datasets
+PIV_data, PLIF_data, xmin, xmax, ymin, ymax = preprocess_old_data(file_PIV, file_PLIF)
+
+# test part
+show_image(PIV_data[:, 1, :, :], PLIF_data[1, :, :],
+           xmin, xmax, ymin, ymax)
+
--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,76 +1,16 @@
+from typing import Tuple
+
 import h5py
 import numpy as np
 import matplotlib as mpl
+import scipy.io
 from matplotlib import pyplot as plt
 
-# PART 1: define the parameters
-file_PIV = str('data/IA_PIV.mat')
-file_PLIF = str('data/IA_PLIF_1to2500.mat')
-
-# PART 2: load the datasets
-with h5py.File(file_PIV, 'r') as file:
-    """
-    Get the PIV numpy array with the shape of (3, 6689, 73, 73), note that:
-        1 denotes the axial(x) velocity, 
-        2 denotes the radial(y) velocity, 
-        3 denotes the tangential(z) velocity
-    """
-    dataset_PIV = file['PIV']['velfield'][:]
-
-    # get the x range of PIV image with (73, 1)
-    PIV_x = file['PIV']['x'][:]
-
-    # get the y range of PIV image with (73, 1)
-    PIV_y = file['PIV']['y'][:]
-
-with h5py.File(file_PLIF, 'r') as file:
-    # get the PLIF numpy array with the shape of (1689, 409, 658)
-    dataset_PLIF = file['PLIF']['PLIFfield'][:]
-
-    # get the x range of PLIF image with (658, 1)
-    PLIF_x = file['PLIF']['x'][:]
-
-    # get the y range of PLIF image with (409, 1)
-    PLIF_y = file['PLIF']['y'][:]
-
-# print(f'min PLIF-x = {PLIF_x.min()}')
-# print(f'max PLIF-x = {PLIF_x.max()}')
-# print(f'min PIV-x = {PIV_x.min()}')
-# print(f'max PIV-x = {PIV_x.max()}')
-#
-# print(f'min PLIF-y = {PLIF_y.min()}')
-# print(f'max PLIF-y = {PLIF_y.max()}')
-# print(f'min PIV-y = {PIV_y.min()}')
-# print(f'max PIV-y = {PIV_y.max()}')
-
-# print(np.shape(dataset_PIV))
-# print(np.shape(dataset_PLIF))
-
-# PART 3: crop the dataset
-# 1. define the range of x, y
-cropped_xmin = max(PLIF_x.min(), PIV_x.min())
-cropped_ymin = max(PLIF_y.min(), PIV_y.min())
-cropped_xmax = min(PLIF_x.max(), PIV_x.max())
-cropped_ymax = min(PLIF_y.max(), PIV_y.max())
-
-# 2. get the indices satisfied the range
-indices_PLIF_x = np.where((PLIF_x >= cropped_xmin) & (PLIF_x <= cropped_xmax))[0]
-indices_PLIF_y = np.where((PLIF_y >= cropped_ymin) & (PLIF_y <= cropped_ymax))[0]
-
-indices_PIV_x = np.where((PIV_x >= cropped_xmin) & (PIV_x <= cropped_xmax))[0]
-indices_PIV_y = np.where((PIV_y >= cropped_ymin) & (PIV_y <= cropped_ymax))[0]
-
-# 3. crop the datasets via the range
-cropped_PIV = dataset_PIV[:, :, indices_PIV_y[:, np.newaxis], indices_PIV_x]
-cropped_PLIF = dataset_PLIF[:, indices_PLIF_y[:, np.newaxis], indices_PLIF_x]
+"""
+This file defines the functions for pre-processing the datasets.
+"""
 
 
-# print(np.shape(cropped_PIV))
-# print(np.shape(cropped_PLIF))
-# print(np.shape(cropped_PIV[:, 999, :, :]))
-# print(np.shape(cropped_PLIF[999, :, :]))
-
-# APPENDIX: related functions used in this file
 def show_image(PIV_image: np.ndarray, PLIF_image: np.ndarray,
                xmin: float, xmax: float, ymin: float, ymax: float) -> None:
     """
@@ -124,5 +64,118 @@ def show_image(PIV_image: np.ndarray, PLIF_image: np.ndarray,
     plt.show()
 
 
-show_image(cropped_PIV[:, 999, :, :], cropped_PLIF[999, :, :],
-           cropped_xmin, cropped_xmax, cropped_ymin, cropped_ymax)
+def preprocess_data(file_PIV: str, file_PLIF: str) \
+        -> Tuple[np.ndarray, np.ndarray, float, float, float, float]:
+
+    """
+    pre-process the dataset provided by the new datasets
+    :param file_PIV: a string represents the filename of PIV image
+    :param file_PLIF: a string represents the filename of PLIF image
+    :return: a numpy array represents the value of preprocessed PIV image,
+            a numpy array represents the value of preprocessed PLIF image,
+            four float numbers denotes xmin, xmax, ymin, ymax of preprocessed data
+    """
+
+    # STEP 1: load the datasets
+    with h5py.File(file_PIV, 'r') as file:
+        """
+        Get the PIV numpy array with the shape of (3, 6689, 73, 73), note that:
+            1 denotes the axial(x) velocity, 
+            2 denotes the radial(y) velocity, 
+            3 denotes the tangential(z) velocity
+        """
+        dataset_PIV = file['PIV']['velfield'][:]
+
+        # get the x range of PIV image with (73, 1)
+        PIV_x = file['PIV']['x'][:]
+
+        # get the y range of PIV image with (73, 1)
+        PIV_y = file['PIV']['y'][:]
+
+    with h5py.File(file_PLIF, 'r') as file:
+        # get the PLIF numpy array with the shape of (1689, 409, 658)
+        dataset_PLIF = file['PLIF']['PLIFfield'][:]
+
+        # get the x range of PLIF image with (658, 1)
+        PLIF_x = file['PLIF']['x'][:]
+
+        # get the y range of PLIF image with (409, 1)
+        PLIF_y = file['PLIF']['y'][:]
+
+    # STEP 2: crop the dataset
+    # 1. define the range of x, y
+    cropped_xmin = max(PLIF_x.min(), PIV_x.min())
+    cropped_ymin = max(PLIF_y.min(), PIV_y.min())
+    cropped_xmax = min(PLIF_x.max(), PIV_x.max())
+    cropped_ymax = min(PLIF_y.max(), PIV_y.max())
+
+    # 2. get the indices satisfied the range
+    indices_PLIF_x = np.where((PLIF_x >= cropped_xmin) & (PLIF_x <= cropped_xmax))[0]
+    indices_PLIF_y = np.where((PLIF_y >= cropped_ymin) & (PLIF_y <= cropped_ymax))[0]
+
+    indices_PIV_x = np.where((PIV_x >= cropped_xmin) & (PIV_x <= cropped_xmax))[0]
+    indices_PIV_y = np.where((PIV_y >= cropped_ymin) & (PIV_y <= cropped_ymax))[0]
+
+    # 3. crop the datasets via the range
+    cropped_PIV = dataset_PIV[:, :, indices_PIV_y[:, np.newaxis], indices_PIV_x]
+    cropped_PLIF = dataset_PLIF[:, indices_PLIF_y[:, np.newaxis], indices_PLIF_x]
+
+    return cropped_PIV, cropped_PLIF, cropped_xmin, cropped_xmax, cropped_ymin, cropped_ymax
+
+
+def preprocess_old_data(file_PIV: str, file_PLIF: str) \
+        -> Tuple[np.ndarray, np.ndarray, float, float, float, float]:
+
+    """
+       pre-process the dataset provided by the old datasets (used by Barwey)
+       :param file_PIV: a string represents the filename of PIV image
+       :param file_PLIF: a string represents the filename of PLIF image
+       :return: a numpy array represents the value of preprocessed PIV image,
+               a numpy array represents the value of preprocessed PLIF image,
+               four float numbers denotes xmin, xmax, ymin, ymax of preprocessed data
+       """
+
+    # STEP 1: load the PIV and PLIF dataset
+    with h5py.File(file_PLIF, 'r') as file:
+        # get the PLIF numpy array
+        dataset_PLIF = file['PLIF']['OH'][:]
+
+        # get the x range of PLIF image with (504, 1)
+        PLIF_x = file['PLIF']['x'][:]
+
+        # get the y range of PLIF image with (833, 1)
+        PLIF_y = file['PLIF']['y'][:]
+
+    PIV_information = scipy.io.loadmat(file_PIV)
+
+    dataset_PIV = PIV_information['PIV']['velfield'][:]
+    dataset_PIV = np.concatenate([np.concatenate(sublist) for sublist in dataset_PIV])
+    dataset_PIV = np.transpose(dataset_PIV)
+
+    PIV_x = PIV_information['PIV']['x'][:]
+    PIV_x = np.concatenate([np.concatenate(sublist) for sublist in PIV_x])
+    PIV_x = np.transpose(PIV_x)
+
+    PIV_y = PIV_information['PIV']['y'][:]
+    PIV_y = np.transpose(np.concatenate([np.concatenate(sublist) for sublist in PIV_y]))
+    # PIV_y = np.transpose(PIV_y)
+
+    # STEP 2: crop the dataset
+    # 1. define the range of x, y
+    cropped_xmin = -15
+    cropped_ymin = 0
+    cropped_xmax = 15
+    cropped_ymax = 30
+
+    # 2. get the indices satisfied the range
+    indices_PLIF_x = np.where((PLIF_x >= cropped_xmin) & (PLIF_x <= cropped_xmax))[0]
+    indices_PLIF_y = np.where((PLIF_y >= cropped_ymin) & (PLIF_y <= cropped_ymax))[0]
+
+    indices_PIV_x = np.where((PIV_x >= cropped_xmin) & (PIV_x <= cropped_xmax))[0]
+    indices_PIV_y = np.where((PIV_y >= cropped_ymin) & (PIV_y <= cropped_ymax))[0]
+
+    # 3. crop the datasets via the range
+    cropped_PIV = dataset_PIV[:, :, indices_PIV_y[:, np.newaxis], indices_PIV_x]
+    cropped_PLIF = dataset_PLIF[:, indices_PLIF_y[:, np.newaxis], indices_PLIF_x]
+
+    return cropped_PIV, cropped_PLIF, cropped_xmin, cropped_xmax, cropped_ymin, cropped_ymax
