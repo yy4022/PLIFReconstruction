7a766d535f3ba51e4c5d8a82c7b266bd21d1db27
Yi Yang <yy4022@ic.ac.uk>
2023-11-27 19:04:49 +0800

    rectify: unify the procedure of splitting datasets in test_main file with the test_show_result file

--- test_main.py
+++ test_main.py
@@ -72,63 +72,63 @@ PIV_y_data2 = preprocess_old_data(cropped_PIV_data2[1, :, :, :], min_PIV_y, max_
 PIV_z_data1 = preprocess_old_data(cropped_PIV_data1[2, :, :, :], min_PIV_z, max_PIV_z)
 PIV_z_data2 = preprocess_old_data(cropped_PIV_data2[2, :, :, :], min_PIV_z, max_PIV_z)
 
-# print(np.shape(PLIF_data1))
-# print(np.shape(PIV_x_data1))
-
-# print(min_PLIF, min_PIV_x, min_PIV_y, min_PIV_z)
-# print(max_PLIF, max_PIV_x, max_PIV_y, max_PIV_z)
-
-# show_box_PIV(PIV_data1[:, :, img_num, :, :], dimension=3, rows=rows, columns=columns) # z-axis
-# show_box_PLIF(PLIF_data1[:, img_num, :, :], rows=rows, columns=columns)
-
 # 2. concatenate the datasets as required
 PIV_x_attached_data = np.concatenate((PIV_x_data1, PIV_x_data2), axis=1) # attached-2000
 PLIF_attached_data = np.concatenate((PLIF_data1, PLIF_data2), axis=1)
 
-# 3. reshape the datasets to 3 dimensions (i.e. flatten the 12 boxes)
-boxes = PIV_x_attached_data.shape[0]
-image_num = PIV_x_attached_data.shape[1]
+# 3. split the datasets for training, validation and testing
+# 3.1. get the total number of data
+attached_num = PIV_x_attached_data.shape[1]
 
-PIV_x_height = PIV_x_attached_data.shape[2]
-PIV_x_width = PIV_x_attached_data.shape[3]
-PIV_x_attached_data = PIV_x_attached_data.reshape((boxes * image_num, PIV_x_height, PIV_x_width))
-
-PLIF_height = PLIF_attached_data.shape[2]
-PLIF_width = PLIF_attached_data.shape[3]
-PLIF_attached_data = PLIF_attached_data.reshape((boxes * image_num, PLIF_height, PLIF_width))
-
-# print(np.shape(PIV_x_attached_data)) # (24000, 14, 11)
-# print(np.shape(PLIF_attached_data)) # (24000, 124, 93)
-
-# 4. split the datasets for training, evaluation and testing
-# 4.1. get the total number of data
-attached_num = PIV_x_attached_data.shape[0]
-
-# 4.2. calculate the position of splitting points to satisfy the proportion (3: 1: 1)
+# 3.2. calculate the position of splitting points to satisfy the proportion (3: 1: 1)
 attached_split_points = [int(np.floor(attached_num * 0.6)), int(np.floor(attached_num * 0.8))]
 
-# 4.3. split the datasets according to the splitting points
-PIV_x_attached_data_split = np.split(PIV_x_attached_data, attached_split_points, axis=0)
-PLIF_attached_data_split = np.split(PLIF_attached_data, attached_split_points, axis=0)
+# 3.3. split the datasets according to the splitting points
+PIV_x_attached_data_split = np.split(PIV_x_attached_data, attached_split_points, axis=1)
+PLIF_attached_data_split = np.split(PLIF_attached_data, attached_split_points, axis=1)
 
-# 4.4. obtain the training, validation, training sets
-training_x_PIV_data = np.expand_dims(PIV_x_attached_data_split[0], axis=1)
-training_PLIF_data = np.expand_dims(PLIF_attached_data_split[0], axis=1)
+# 3.4. obtain the training, validation, testing sets
+training_x_PIV_data = PIV_x_attached_data_split[0]
+training_PLIF_data = PLIF_attached_data_split[0]
+training_nums = training_PLIF_data.shape[1]
 
-validation_x_PIV_data = np.expand_dims(PIV_x_attached_data_split[1], axis=1)
-validation_PLIF_data = np.expand_dims(PLIF_attached_data_split[1], axis=1)
+validation_x_PIV_data = PIV_x_attached_data_split[1]
+validation_PLIF_data = PLIF_attached_data_split[1]
+validation_nums = validation_PLIF_data.shape[1]
 
-testing_x_PIV_data = np.expand_dims(PIV_x_attached_data_split[2], axis=1)
-testing_PLIF_data = np.expand_dims(PLIF_attached_data_split[2], axis=1)
+testing_x_PIV_data = PIV_x_attached_data_split[2]
+testing_PLIF_data = PLIF_attached_data_split[2]
+testing_nums = testing_PLIF_data.shape[1]
 
-# print(np.shape(training_x_PIV_data))
-# print(np.shape(training_PLIF_data))
-#
-# print(np.shape(validation_x_PIV_data))
-# print(np.shape(validation_PLIF_data))
-#
-# print(np.shape(testing_x_PIV_data))
-# print(np.shape(testing_PLIF_data))
+# 3.5. reshape the training, validation, testing datasets
+# get the essential shape information for reshaping datasets
+boxes = training_x_PIV_data.shape[0] # 12 boxes
+
+PIV_x_height = training_x_PIV_data.shape[2]
+PIV_x_width = training_x_PIV_data.shape[3]
+
+PLIF_height = training_PLIF_data.shape[2]
+PLIF_width = training_PLIF_data.shape[3]
+
+# then reshape datasets (i.e. flatten the 12 boxes)
+training_x_PIV_data = training_x_PIV_data.reshape((boxes * training_nums, PIV_x_height, PIV_x_width))
+training_PLIF_data = training_PLIF_data.reshape((boxes * training_nums, PLIF_height, PLIF_width))
+
+validation_x_PIV_data = validation_x_PIV_data.reshape((boxes * validation_nums, PIV_x_height, PIV_x_width))
+validation_PLIF_data = validation_PLIF_data.reshape((boxes * validation_nums, PLIF_height, PLIF_width))
+
+testing_x_PIV_data = testing_x_PIV_data.reshape((boxes * testing_nums, PIV_x_height, PIV_x_width))
+testing_PLIF_data = testing_PLIF_data.reshape((boxes * testing_nums, PLIF_height, PLIF_width))
+
+# 3.6. obtain the training, validation, training sets
+training_x_PIV_data = np.expand_dims(training_x_PIV_data, axis=1)
+training_PLIF_data = np.expand_dims(training_PLIF_data, axis=1)
+
+validation_x_PIV_data = np.expand_dims(validation_x_PIV_data, axis=1)
+validation_PLIF_data = np.expand_dims(validation_PLIF_data, axis=1)
+
+testing_x_PIV_data = np.expand_dims(testing_x_PIV_data, axis=1)
+testing_PLIF_data = np.expand_dims(testing_PLIF_data, axis=1)
 
 # 4.5. create the corresponding datasets
 training_x_PIV_dataset = MyDataset(training_x_PIV_data)
