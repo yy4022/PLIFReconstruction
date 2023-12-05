44a16c88f84b84b20684532d0c60b2a6ed4bdd7e
Yi Yang <yy4022@ic.ac.uk>
2023-12-05 15:38:11 +0800

    update: the local fullyCNN models for 12 boxes

--- main_local.py
+++ main_local.py
@@ -36,7 +36,7 @@ EPOCHS = 1000
 lr = 0.0001
 if_existing = False  # a flag recording if there is an existing fullyCNN model
 dataset_num = 4
-specified_box = 1  # indicates which specified box corresponds to the local fullyCNN model, range in [1, 12]
+specified_box = 12  # indicates which specified box corresponds to the local fullyCNN model, range in [1, 12]
 
 # PART 2：create the dataloader for training the model of the specified box
 # 2.1. read the specified box image data from all data files
--- show_result_local.py
+++ show_result_local.py
@@ -1,7 +1,18 @@
-
+import torch
 
 """
 This file is used for showing the results via the trained global model.
 """
+
+# PART 1: define the parameters
+# 1.1. choose the device
+torch.cuda.set_device(0)
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(f"Selected device: {device}")
+
+
 
 
