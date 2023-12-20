4a645d89d194c2c48611b84b12cc3c24557c1847
yy4022@ic.ac.uk <yy4022@ic.ac.uk>
2023-12-20 14:28:32 +0800

    implement: preprocess the PLIF and PIV datasets separately

--- preprocessed_PIV_datasets.py
+++ preprocessed_PIV_datasets.py
@@ -1,6 +1,8 @@
 import json
 
-from methods_preprocess import get_min_max, crop_old_PIVdata
+import numpy as np
+
+from methods_preprocess import get_min_max, crop_old_PIVdata, concatenate_data, preprocess_data_list
 
 """
 SECTION 1: This section is used for getting the global min and max values of PIV dataset for normalization.
@@ -8,38 +10,92 @@ SECTION 1: This section is used for getting the global min and max values of PIV
         2. you can comment this section after implementing it)
 """
 
-# PART 1: define the parameters for training the model
-# provide the filename of PIV data
+# # PART 1: define the parameters for training the model
+# # provide the filename of PIV data
+# files_PIV = [
+#     'data/Full dataset/D1F1_air240_PIV_1to14999.mat',
+# ]
+#
+# # PART 2: get the min and max value for this specified dataset (for PIV)
+# # 2.1. preprocess the datasets, then return the cropped datasets
+# cropped_PIV_x_data, cropped_PIV_y_data, cropped_PIV_z_data = crop_old_PIVdata(files_PIV)
+#
+# # 2.2. get the min and max value for this PIV-x, y, z dataset
+# min_PIV_x, max_PIV_x = get_min_max(cropped_PIV_x_data)
+# min_PIV_y, max_PIV_y = get_min_max(cropped_PIV_y_data)
+# min_PIV_z, max_PIV_z = get_min_max(cropped_PIV_z_data)
+#
+# # PART 3: compare the min and max value with the saved one
+# existing_data = {}
+#
+# # add new information to the file
+# new_data = {
+#     'min_PIV_x': float(min_PIV_x),
+#     'max_PIV_x': float(max_PIV_x),
+#     'min_PIV_y': float(min_PIV_y),
+#     'max_PIV_y': float(max_PIV_y),
+#     'min_PIV_z': float(min_PIV_z),
+#     'max_PIV_z': float(max_PIV_z),
+# }
+#
+# existing_data.update(new_data)
+#
+# print(existing_data)
+#
+# # 3.2. save the updated data information
+# with open('data/Preprocessed_Data_Fulldataset/dataset_information_PIV.json', 'w') as file:
+#     json.dump(existing_data, file)
+
+"""
+SECTION 2: This section is used for cropping, normalizing and discretizing all PIV datasets.
+NOTE: 1. the result data would be stored in the same directory.
+      2. the whole dataset will be split for getting the unified format with PLIF datasets.
+"""
+
+# PART 1: provide the essential information
+# 1.1. define the parameters
+dataset_nums = 8
+
+# 1.2. provide the filename of PIV data
 files_PIV = [
     'data/Full dataset/D1F1_air240_PIV_1to14999.mat',
 ]
 
-# PART 2: get the min and max value for this specified dataset (for PIV)
-# 2.1. preprocess the datasets, then return the cropped datasets
+# PART 2: get the global min and max value
+with open('data/Preprocessed_Data_Fulldataset/dataset_information_PIV.json', 'r') as file:
+    existing_data = json.load(file)
+
+min_PIV_x = np.float32(existing_data['min_PIV_x'])
+max_PIV_x = np.float32(existing_data['max_PIV_x'])
+min_PIV_y = np.float32(existing_data['min_PIV_y'])
+max_PIV_y = np.float32(existing_data['max_PIV_y'])
+min_PIV_z = np.float32(existing_data['min_PIV_z'])
+max_PIV_z = np.float32(existing_data['max_PIV_z'])
+
+# PART 3:  preprocess the datasets (crop, normalize and discretize)
 cropped_PIV_x_data, cropped_PIV_y_data, cropped_PIV_z_data = crop_old_PIVdata(files_PIV)
 
-# 2.2. get the min and max value for this PIV-x, y, z dataset
-min_PIV_x, max_PIV_x = get_min_max(cropped_PIV_x_data)
-min_PIV_y, max_PIV_y = get_min_max(cropped_PIV_y_data)
-min_PIV_z, max_PIV_z = get_min_max(cropped_PIV_z_data)
+# normalize and discretize the datasets according to the min, max values
+preprocessed_PIV_x_data = preprocess_data_list(cropped_PIV_x_data, min_PIV_x, max_PIV_x)
+preprocessed_PIV_y_data = preprocess_data_list(cropped_PIV_y_data, min_PIV_y, max_PIV_y)
+preprocessed_PIV_z_data = preprocess_data_list(cropped_PIV_z_data, min_PIV_z, max_PIV_z)
 
-# PART 3: compare the min and max value with the saved one
-existing_data = {}
+PIV_x_data = concatenate_data(preprocessed_PIV_x_data)
+PIV_y_data = concatenate_data(preprocessed_PIV_y_data)
+PIV_z_data = concatenate_data(preprocessed_PIV_z_data)
 
-# add new information to the file
-new_data = {
-    'min_PIV_x': float(min_PIV_x),
-    'max_PIV_x': float(max_PIV_x),
-    'min_PIV_y': float(min_PIV_y),
-    'max_PIV_y': float(max_PIV_y),
-    'min_PIV_z': float(min_PIV_z),
-    'max_PIV_z': float(max_PIV_z),
-}
+# PART 4: save the specified dataset
+for i in range(dataset_nums):
+    start_num = i * 2000
+    end_num = start_num + 2000
 
-existing_data.update(new_data)
+    if end_num > PIV_x_data.shape[1]:
+        end_num = PIV_x_data.shape[1]
 
-print(existing_data)
+    PIV_x_data_i = PIV_x_data[:, start_num:end_num, :, :]
+    PIV_y_data_i = PIV_y_data[:, start_num:end_num, :, :]
+    PIV_z_data_i = PIV_z_data[:, start_num:end_num, :, :]
 
-# 3.2. save the updated data information
-with open('data/Preprocessed_Data_Fulldataset/dataset_information_PIV.json', 'w') as file:
-    json.dump(existing_data, file)
+    np.save(f'data/Full dataset/PIV_x_dataset{i + 1}.npy', PIV_x_data_i)
+    np.save(f'data/Full dataset/PIV_y_dataset{i + 1}.npy', PIV_y_data_i)
+    np.save(f'data/Full dataset/PIV_z_dataset{i + 1}.npy', PIV_z_data_i)
--- preprocessed_PLIF_datasets.py
+++ preprocessed_PLIF_datasets.py
@@ -1,6 +1,8 @@
 import json
 
-from methods_preprocess import crop_old_PLIFdata, get_min_max
+import numpy as np
+
+from methods_preprocess import crop_old_PLIFdata, get_min_max, preprocess_data_list, concatenate_data
 
 """
 SECTION 1: This section is used for getting the global min and max values of PLIF dataset for normalization.
@@ -8,8 +10,69 @@ SECTION 1: This section is used for getting the global min and max values of PLI
         2. you can comment this section after implementing it)
 """
 
-# PART 1: define the parameters for training the model
-# provide the filename of PLIF data
+# # PART 1: define the parameters for training the model
+# # provide the filename of PLIF data
+# files_PLIF = [
+#     # 'data/PLIF240air/D1F1_air240_PLIF_1to2000.mat',  # training + validation - dataset1 (2000)
+#     # 'data/PLIF240air/D1F1_air240_PLIF_2001to4000.mat',  # training + validation - dataset2 (2000)
+#     # 'data/PLIF240air/D1F1_air240_PLIF_4001to6000.mat',  # training + validation - dataset3 (2000)
+#     # 'data/PLIF240air/D1F1_air240_PLIF_6001to8000.mat',  # training + validation - dataset4 (2000)
+#     # 'data/PLIF240air/D1F1_air240_PLIF_8001to10000.mat',  # training + validation - dataset5 (2000)
+#     # 'data/PLIF240air/D1F1_air240_PLIF_10001to12000.mat',  # training + validation - dataset6 (2000)
+#     # 'data/PLIF240air/D1F1_air240_PLIF_12001to14000.mat',  # training + validation - dataset7 (2000)
+#     'data/PLIF240air/D1F1_air240_PLIF_14001to14999.mat',  # testing - dataset8 (999)
+# ]
+#
+# # PART 2: get the min and max value for this specified dataset (for PLIF)
+# # 2.1. preprocess the datasets, then return the cropped datasets
+# cropped_PLIF_data = crop_old_PLIFdata(files_PLIF)
+#
+# # 2.2. get the min and max value for this PLIF dataset
+# min_PLIF, max_PLIF = get_min_max(cropped_PLIF_data)
+#
+# # PART 3: compare the min and max value with the saved one
+# # 3.1. try to load the existing file
+# try:
+#     # if the values have existed, compare and update the value
+#     with open('data/Preprocessed_Data_Fulldataset/dataset_information_PLIF.json', 'r') as file:
+#         existing_data = json.load(file)
+#
+#     current_min_PLIF = existing_data['min_PLIF']
+#     current_max_PLIF = existing_data['max_PLIF']
+#
+#     if current_min_PLIF > float(min_PLIF):
+#         existing_data['min_PLIF'] = float(min_PLIF)
+#     if current_max_PLIF < float(min_PLIF):
+#         existing_data['max_PLIF'] = float(min_PLIF)
+#
+# except FileNotFoundError:
+#     # if the values have not existed, create a new one
+#     existing_data = {}
+#
+#     # add new information to the file
+#     new_data = {
+#         'min_PLIF': float(min_PLIF),
+#         'max_PLIF': float(max_PLIF),
+#     }
+#
+#     existing_data.update(new_data)
+#
+# print(existing_data)
+#
+# # 3.2. save the updated data information
+# with open('data/Preprocessed_Data_Fulldataset/dataset_information_PLIF.json', 'w') as file:
+#     json.dump(existing_data, file)
+
+"""
+SECTION 2: This section is used for cropping, normalizing and discretizing all PLIF datasets.
+NOTE: the result data would be stored in the same directory.
+"""
+
+# PART 1: provide the essential information
+# 1.1. define the parameters
+specified_dataset = 8
+
+# 1.2. provide the filename of PLIF data
 files_PLIF = [
     # 'data/PLIF240air/D1F1_air240_PLIF_1to2000.mat',  # training + validation - dataset1 (2000)
     # 'data/PLIF240air/D1F1_air240_PLIF_2001to4000.mat',  # training + validation - dataset2 (2000)
@@ -21,42 +84,21 @@ files_PLIF = [
     'data/PLIF240air/D1F1_air240_PLIF_14001to14999.mat',  # testing - dataset8 (999)
 ]
 
-# PART 2: get the min and max value for this specified dataset (for PLIF)
-# 2.1. preprocess the datasets, then return the cropped datasets
+# PART 2: get the global min and max value
+with open('data/Preprocessed_Data_Fulldataset/dataset_information_PLIF.json', 'r') as file:
+    existing_data = json.load(file)
+
+min_PLIF = np.float32(existing_data['min_PLIF'])
+max_PLIF = np.float32(existing_data['max_PLIF'])
+
+# PART 3:  preprocess the datasets (crop, normalize and discretize)
 cropped_PLIF_data = crop_old_PLIFdata(files_PLIF)
 
-# 2.2. get the min and max value for this PLIF dataset
-min_PLIF, max_PLIF = get_min_max(cropped_PLIF_data)
+# normalize and discretize the datasets according to the min, max values
+preprocessed_PLIF_data = preprocess_data_list(cropped_PLIF_data, min_PLIF, max_PLIF)
 
-# PART 3: compare the min and max value with the saved one
-# 3.1. try to load the existing file
-try:
-    # if the values have existed, compare and update the value
-    with open('data/Preprocessed_Data_Fulldataset/dataset_information_PLIF.json', 'r') as file:
-        existing_data = json.load(file)
+# concatenate the datasets as required
+PLIF_data = concatenate_data(preprocessed_PLIF_data)
 
-    current_min_PLIF = existing_data['min_PLIF']
-    current_max_PLIF = existing_data['max_PLIF']
-
-    if current_min_PLIF > float(min_PLIF):
-        existing_data['min_PLIF'] = float(min_PLIF)
-    if current_max_PLIF < float(min_PLIF):
-        existing_data['max_PLIF'] = float(min_PLIF)
-
-except FileNotFoundError:
-    # if the values have not existed, create a new one
-    existing_data = {}
-
-    # add new information to the file
-    new_data = {
-        'min_PLIF': float(min_PLIF),
-        'max_PLIF': float(max_PLIF),
-    }
-
-    existing_data.update(new_data)
-
-print(existing_data)
-
-# 3.2. save the updated data information
-with open('data/Preprocessed_Data_Fulldataset/dataset_information_PLIF.json', 'w') as file:
-    json.dump(existing_data, file)
+# PART 4: save this specified data
+np.save(f'data/PLIF240air/PLIF_dataset{specified_dataset}.npy', PLIF_data)
